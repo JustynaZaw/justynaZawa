24647107a5ae6c2c6299990529de1440e2ad1b1d
JustynaWSB <Justyna@wsb.pl>
2023-01-14 14:45:12 +0100

    m

--- arq_kwarq.py
+++ arq_kwarq.py
@@ -2,8 +2,13 @@
 def multiply(*args):
     print(args[0])
     print(args[-1])
+    print(len(args))
+    for i in args:
+        if i > 6:
+            print(f" zarabia za duzo, bo az {i}")
 
 
+multiply(5, 6, 7, 12, 1)
 
 
 
