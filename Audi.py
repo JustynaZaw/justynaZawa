a4eb553a3a9e2b9d80f039594d0c0560daa4d129
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-01-15 11:20:26 +0100

    s

--- Audi.py
+++ Audi.py
@@ -1,20 +1,40 @@
 class Audi:
-    def __init__(self):
-        self.kolor  = "czerwony"
+    def __init__(self, kolor, kondycja):
+        self.kolor  = kolor
         self.ilosc_paliwa = 10
-        self.kondycja = 4
+        self.kondycja = kondycja
+        self.tryb_ekonomiczny = False
+        self.spalanie_na_100 = 14
     def __str__(self):
         napis = (f"Audi ma kolor {self.kolor} i jest w kondycji {self.kondycja}")
         return napis
+    def zasieg(self):
+        zasieg  = self.ilosc_paliwa / self.spalanie_na_100 * 100
+        return round(zasieg)
+    def ustaw_tryb(self, tryb):
+       self.tryb = tryb
+       if self.tryb == "eco":
+            self.spalanie_na_100 = 10
+            self.tryb_ekonomiczny = True
+            print("tryb eco")
+       elif self.tryb == "normal":
+            self.spalanie_na_100 = 14
+            self.tryb_ekonomiczny = False
+            print("tryb normal")
+       else:
+           print("tryb nierozpoznany")
+    def wlacz_eco(self):
+        self.spalanie_na_100 = 10
+        self.tryb_ekonomiczny = True
+        print("tryb eco wlaczony")
+    def wlacz_normal(self):
+        self.spalanie_na_100 = 14
+        self.tryb_ekonomiczny = False
+        print("tryb normal wlaczony")
 
 
-moje_auto = Audi()
-print(moje_auto.ilosc_paliwa)
-print(moje_auto.kondycja)
-moje_auto.kondycja += 1
-print(moje_auto.kondycja)
+moje_auto = Audi("czerwony", 4)
+auto_sasiada = Audi("zielony", 5)
 
-auto_sasiada = Audi()
-print(auto_sasiada.kondycja)
 
-print(moje_auto)
+print(moje_auto.ustaw_tryb("eco"))
